98964ce74c3b5f84f5799290379a67061e4bb969
Rohan <rohandute1@gmail.com>
2023-06-08 21:50:25 +0530

    [Rohan] Adding Welcome message successfully

---
+++ 4B_OOP_Problem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4B_OOP_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to OOP's Practice problems");
+            
+            Console.ReadLine();
+        }
+    }
+}
