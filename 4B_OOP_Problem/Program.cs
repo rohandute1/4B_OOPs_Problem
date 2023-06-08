957cdfc55bd8c4bbbec48d550ea5528ba83f0bc9
Rohan <rohandute1@gmail.com>
2023-06-08 22:40:40 +0530

    [Rohan] Print unique elements only created successfully

--- 4B_OOP_Problem/Program.cs
+++ 4B_OOP_Problem/Program.cs
@@ -11,14 +11,24 @@ namespace _4B_OOP_Problem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to OOP's Practice problems");
+            Console.WriteLine("Please select any one option from following.");
+            Console.WriteLine(" 1.Duplicate Numbers\n 2.Unique Element Printer\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    int[] array = { 1, 2, 3, 4, 4, 5, 6, 7, 7, 7, 8, 9, 9, 9 };
+                    DuplicateCounter counter = new DuplicateCounter(array);
+                    int totalDuplicates = counter.CountDuplicates();
+                    Console.WriteLine("Total number of duplicate elements: " + totalDuplicates);
+                    break;
 
-            int[] array = { 1, 2, 3, 4, 4, 5, 6, 7, 7, 7, 8, 9, 9, 9 };
-
-            DuplicateCounter counter = new DuplicateCounter(array);
-            int totalDuplicates = counter.CountDuplicates();
-
-            Console.WriteLine("Total number of duplicate elements: " + totalDuplicates);
-
+                case 2:
+                    int[] ary = { 1, 2, 3, 4, 4, 5, 6, 7, 7, 7, 8, 9, 9, 9 };
+                    UniqueElementPrinter printer = new UniqueElementPrinter(ary);
+                    printer.PrintUniqueElements();
+                    break;
+            }
             Console.ReadLine();
         }
     }
---
+++ 4B_OOP_Problem/UniqueElementPrinter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4B_OOP_Problem
+{
+    internal class UniqueElementPrinter
+    {
+        private int[] ary;
+
+        public UniqueElementPrinter(int[] array)
+        {
+            this.ary = array;
+        }
+
+        public void PrintUniqueElements()
+        {
+            Dictionary<int, int> countDictionary = new Dictionary<int, int>();
+
+            foreach (int element in ary)
+            {
+                if (countDictionary.ContainsKey(element))
+                {
+                    countDictionary[element]++;
+                }
+                else
+                {
+                    countDictionary[element] = 1;
+                }
+            }
+
+            Console.WriteLine("Unique elements in the array:");
+
+            foreach (var pair in countDictionary)
+            {
+                if (pair.Value == 1)
+                {
+                    Console.WriteLine(pair.Key);
+                }
+            }
+        }
+    }
+}
