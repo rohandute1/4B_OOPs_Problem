1bfc904a97599b1d953cfabb94bba51bb67d6ff7
Rohan <rohandute1@gmail.com>
2023-06-08 22:05:24 +0530

    [Rohan]Successfull creation of total duplicate number counter

---
+++ 4B_OOP_Problem/DuplicateCounter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4B_OOP_Problem
+{
+    internal class DuplicateCounter
+    {
+        private readonly int[] array;
+        private readonly Dictionary<int, int> countDictionary;
+
+        public DuplicateCounter(int[] array)
+        {
+            this.array = array;
+            countDictionary = new Dictionary<int, int>();
+        }
+
+        public int CountDuplicates()
+        {
+            foreach (int element in array)
+            {
+                if (countDictionary.ContainsKey(element))
+                {
+                    countDictionary[element]++;
+                }
+                else
+                {
+                    countDictionary[element] = 1;
+                }
+            }
+
+            int totalDuplicates = 0;
+            foreach (var pair in countDictionary)
+            {
+                if (pair.Value > 1)
+                {
+                    totalDuplicates += pair.Value - 1;
+                }
+            }
+
+            return totalDuplicates;
+        }
+    }
+}
--- 4B_OOP_Problem/Program.cs
+++ 4B_OOP_Problem/Program.cs
@@ -11,7 +11,14 @@ namespace _4B_OOP_Problem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to OOP's Practice problems");
-            
+
+            int[] array = { 1, 2, 3, 4, 4, 5, 6, 7, 7, 7, 8, 9, 9, 9 };
+
+            DuplicateCounter counter = new DuplicateCounter(array);
+            int totalDuplicates = counter.CountDuplicates();
+
+            Console.WriteLine("Total number of duplicate elements: " + totalDuplicates);
+
             Console.ReadLine();
         }
     }
