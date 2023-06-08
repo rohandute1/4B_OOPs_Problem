a507173aca511050edcb2076d96b68595003e5ce
Rohan <rohandute1@gmail.com>
2023-06-08 22:53:44 +0530

    [Rohan] Frequency of elements counter created successfully

---
+++ 4B_OOP_Problem/ElementFrequencyCounter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4B_OOP_Problem
+{
+    internal class ElementFrequencyCounter
+    {
+        private int[] aray;
+        private Dictionary<int, int> frequencyDictionary;
+
+        public ElementFrequencyCounter(int[] array)
+        {
+            this.aray = array;
+            frequencyDictionary = new Dictionary<int, int>();
+        }
+
+        public void CountElementFrequency()
+        {
+            foreach (int element in aray)
+            {
+                if (frequencyDictionary.ContainsKey(element))
+                {
+                    frequencyDictionary[element]++;
+                }
+                else
+                {
+                    frequencyDictionary[element] = 1;
+                }
+            }
+        }
+
+        public void PrintElementFrequency()
+        {
+            Console.WriteLine("Element Frequency:");
+
+            foreach (var pair in frequencyDictionary)
+            {
+                Console.WriteLine(pair.Key + " - " + pair.Value);
+            }
+        }
+    }
+}
--- 4B_OOP_Problem/Program.cs
+++ 4B_OOP_Problem/Program.cs
@@ -12,7 +12,7 @@ namespace _4B_OOP_Problem
         {
             Console.WriteLine("Welcome to OOP's Practice problems");
             Console.WriteLine("Please select any one option from following.");
-            Console.WriteLine(" 1.Duplicate Numbers\n 2.Unique Element Printer\n");
+            Console.WriteLine(" 1.Duplicate Numbers\n 2.Unique Element Printer\n 3.Count Frequency of Elements\n");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -28,6 +28,13 @@ namespace _4B_OOP_Problem
                     UniqueElementPrinter printer = new UniqueElementPrinter(ary);
                     printer.PrintUniqueElements();
                     break;
+                case 3:
+                    int[] aray = { 1, 2, 3, 4, 4, 5, 6, 7, 7, 7, 8, 9, 9, 9 };
+                    ElementFrequencyCounter count = new ElementFrequencyCounter(aray);
+                    count.CountElementFrequency();
+                    count.PrintElementFrequency();
+                    break;
+
             }
             Console.ReadLine();
         }
