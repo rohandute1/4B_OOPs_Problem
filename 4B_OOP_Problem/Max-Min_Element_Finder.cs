b9c860411e10389d2650f87071cd1db037f8e714
Rohan <rohandute1@gmail.com>
2023-06-13 14:07:29 +0530

    [Rohan] Successfully Added Max-Min Element Finder

---
+++ 4B_OOP_Problem/Max-Min_Element_Finder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4B_OOP_Problem
+{
+    internal class Max_Min_Element_Finder
+    {
+        private int[] ar;
+        private int maximum;
+        private int minimum;
+
+        public Max_Min_Element_Finder(int[] ar)
+        {
+            this.ar = ar;
+            maximum = int.MinValue;
+            minimum = int.MaxValue;
+        }
+
+        public void FindMinMax()
+        {
+            foreach (int element in ar)
+            {
+                if (element > maximum)
+                {
+                    maximum = element;
+                }
+
+                if (element < minimum)
+                {
+                    minimum = element;
+                }
+            }
+        }
+
+        public int GetMaximum()
+        {
+            return maximum;
+        }
+
+        public int GetMinimum()
+        {
+            return minimum;
+        }
+    }
+}
--- 4B_OOP_Problem/Program.cs
+++ 4B_OOP_Problem/Program.cs
@@ -12,7 +12,7 @@ namespace _4B_OOP_Problem
         {
             Console.WriteLine("Welcome to OOP's Practice problems");
             Console.WriteLine("Please select any one option from following.");
-            Console.WriteLine(" 1.Duplicate Numbers\n 2.Unique Element Printer\n 3.Count Frequency of Elements\n");
+            Console.WriteLine(" 1.Duplicate Numbers\n 2.Unique Element Printer\n 3.Count Frequency of Elements\n 4.ArrayMinMaxFinder");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -34,6 +34,19 @@ namespace _4B_OOP_Problem
                     count.CountElementFrequency();
                     count.PrintElementFrequency();
                     break;
+                case 4:
+                    int[] ar = { 7, 2, 9, 1, 5, 4, 6, 3, 8 };
+
+                    Max_Min_Element_Finder finder = new Max_Min_Element_Finder(ar);
+                    finder.FindMinMax();
+
+                    int maximum = finder.GetMaximum();
+                    int minimum = finder.GetMinimum();
+
+                    Console.WriteLine("Maximum element: " + maximum);
+                    Console.WriteLine("Minimum element: " + minimum);
+                    break;
+
 
             }
             Console.ReadLine();
